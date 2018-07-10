4d3b641643b1bd0a2259edd263d27036f17fb3db
Eric Chan <ericchan@Erics-MacBook-Pro.local>
2018-07-09 21:20:27 -0400

    added content length remover

---
+++ src/main/java/com/demo/restful/config/ContentLengthHeaderRemover.java
@@ -0,0 +1,19 @@
+package com.rbc.midtier.arrangements.config;
+
+import java.io.IOException;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpRequestInterceptor;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.protocol.HttpContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ContentLengthHeaderRemover implements HttpRequestInterceptor{
+
+	@Override
+    public void process(HttpRequest request, HttpContext context) throws HttpException, IOException {
+        request.removeHeaders(HTTP.CONTENT_LEN);// fighting org.apache.http.protocol.RequestContent's ProtocolException("Content-Length header already present");
+    }
+}
